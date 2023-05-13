78f7a6cfe58096df3063de9a67e3d410539a4aac
AlphaKR93 <dev@alpha93.kr>
2023-05-13 14:17:08 +0900

    Fix support notification, Fix millisecond cookie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,10 @@ export default function IndexPage() {
                 <RemainBox />
                 <div className={styles.support}>
                     <InformationCircleIcon width={24} height={24} />
-                    <p className={styles.content}>
-                        생일 추가, 오류 신고는
-                        <Link href="https://www.instagram.com/alphakr93/">인스타그램 DM</Link>또는
-                        <Link href="https://open.kakao.com/me/alphakr93_education">카카오톡 오픈채팅</Link>
-                        으로 문의하실 수 있습니다.
+                    <p>
+                        생일 추가, 오류 신고는 <Link href="https://www.instagram.com/alphakr93/">인스타그램 DM</Link>{" "}
+                        또는 <Link href="https://open.kakao.com/me/alphakr93_education">카카오톡 오픈채팅</Link> 으로
+                        문의하실 수 있습니다.
                     </p>
                 </div>
                 <section className={styles.main}>
--- src/components/store/provider.tsx
+++ src/components/store/provider.tsx
@@ -6,6 +6,7 @@ import { getCookie } from "cookies-next";
 import { setEnabledDdayTypes } from "@/components/store/reducer/dday/enabled_types.slice";
 import { setEnabledDdayGrades } from "@/components/store/reducer/dday/enabled_grades.slice";
 import { setEnabledDdaySemesters } from "@/components/store/reducer/dday/enabled_semester.slice";
+import { setMillisecondStatus } from "@/components/store/reducer/dday/is_millisecond_enabled.reducer";
 import store, { useAppDispatch } from "./store";
 
 function LoadCookies() {
@@ -14,11 +15,13 @@ function LoadCookies() {
     const enabledDdayTypes = getCookie("enabled_dday_types");
     const enabledDdayGrades = getCookie("enabled_dday_grades");
     const enabledDdaySemesters = getCookie("enabled_dday_semesters");
+    const isMillisecondEnabled = getCookie("is_millisecond_enabled");
 
     if (typeof enabledDdayTypes === "string") dispatch(setEnabledDdayTypes(enabledDdayTypes.split(" ")));
     if (typeof enabledDdayGrades === "string")
         dispatch(setEnabledDdayGrades(enabledDdayGrades.split(" ").map(grade => Number(grade))));
     if (typeof enabledDdaySemesters === "string") dispatch(setEnabledDdaySemesters(enabledDdaySemesters.split(" ")));
+    if (typeof isMillisecondEnabled === "string") dispatch(setMillisecondStatus(isMillisecondEnabled === "true"));
 
     return <div />;
 }
