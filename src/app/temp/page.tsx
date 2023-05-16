3c8ccd78b25d8b0e2e55f5778f2ffb8b5b52a42d
Alpha <alphakr93@outlook.com>
2023-05-16 03:41:26 +0000

    Update page.tsx

--- src/app/temp/page.tsx
+++ src/app/temp/page.tsx
@@ -1,7 +1,8 @@
 export default async function RiverTemperatureCheckPage() {
-    const response = (
-        await fetch("http://openapi.seoul.go.kr:8088/7565624364616c703739686b575171/json/WPOSInformationTime/1/5/")
-    ).json();
+    const response = await fetch(
+        "http://openapi.seoul.go.kr:8088/7565624364616c703739686b575171/json/WPOSInformationTime/1/5/"
+    );
+    if (response.ok) data = response.json();
 
-    return <main>{response}</main>;
+    return <main>{data}</main>;
 }
